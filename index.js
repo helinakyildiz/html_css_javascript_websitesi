20f28cf129c702c8f70c521ed33c6df9437eb098
Helin Akyıldız <101362099+helinakyildiz@users.noreply.github.com>
2024-08-19 18:35:44 +0300

    html_css_javascript_websitesi

---
+++ index.js
@@ -0,0 +1,26 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    if (n >= slides.length) {slideIndex = 0}
+    if (n < 0) {slideIndex = slides.length - 1}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex].style.display = "block";  
+}
+
+setInterval(function() {
+    plusSlides(1);
+}, 3000); // 3 saniye sonra otomatik olarak bir sonraki slayda geç
+
+
+
+   
+
